9dfdd7361ab54eed989e85ee09a2af6e2e1929bc
cambo Guo <76270078+Skywalker2333@users.noreply.github.com>
2025-05-11 20:58:07 +0800

    Add files via upload

---
+++ go1.bat
@@ -0,0 +1,17 @@
+@echo off
+:: 检查是否已经添加到开机自启
+reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "AutoStartFrpAndOne" >nul 2>&1
+if %errorLevel% equ 0 (
+    goto startPrograms
+) else (
+    :: 获取当前批处理文件的完整路径
+    set "currentPath=%~dp0%~nx0"
+    :: 添加到开机自启项
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "AutoStartFrpAndOne" /t REG_SZ /d "%currentPath%" /f
+)
+
+:startPrograms
+:: 启动frp.exe
+start "" "frp.exe"
+:: 启动1.exe
+start "" "1.exe"
