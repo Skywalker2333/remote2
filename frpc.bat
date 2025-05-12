778798c31ee085bc8418eef17d6fecc42091d9f4
cambo Guo <76270078+Skywalker2333@users.noreply.github.com>
2025-05-12 22:56:35 +0800

    Update frpc.bat

--- frpc.bat
+++ frpc.bat
@@ -6,9 +6,7 @@ rem ======================================================================
 set "frpcPath=C:\gj\guojam\is\handsome\remote2-3\frpc.exe"
 set "configFile=C:\gj\guojam\is\handsome\remote2-3\frpc.ini"
 
-rem ----------------------------------------------------------------------
-rem 
-rem ----------------------------------------------------------------------
+
 if not exist %frpcPath% (
     echo [错误] frpc.exe 未找到！请检查路径：
     echo %frpcPath%
@@ -23,17 +21,12 @@ if not exist %configFile% (
     exit /b 1
 )
 
-rem ----------------------------------------------------------------------
-rem 
-rem ----------------------------------------------------------------------
+
 echo 
 echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\hide_window.vbs"
 echo WshShell.Run "%frpcPath% -c %configFile%", 0, False >> "%temp%\hide_window.vbs"
 cscript //nologo "%temp%\hide_window.vbs" >nul 2>&1
 del "%temp%\hide_window.vbs" /f /q
 
-rem ----------------------------------------------------------------------
-rem 
-rem ----------------------------------------------------------------------
 echo 
 exit /b 0
